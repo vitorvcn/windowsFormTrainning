692dacb805a4f3c72e49a4f479088116a1b715e7
Vitor Nascimento <vi_vcn@hotmail.com>
2023-05-24 01:08:41 -0300

    Exercise 2

---
+++ Exercicio02/Exercicio02/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Exercicio02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLado = new System.Windows.Forms.Label();
+            this.txtLado = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLado
+            // 
+            this.lblLado.AutoSize = true;
+            this.lblLado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLado.Location = new System.Drawing.Point(43, 28);
+            this.lblLado.Name = "lblLado";
+            this.lblLado.Size = new System.Drawing.Size(60, 26);
+            this.lblLado.TabIndex = 0;
+            this.lblLado.Text = "Lado";
+            this.lblLado.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtLado
+            // 
+            this.txtLado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLado.Location = new System.Drawing.Point(43, 57);
+            this.txtLado.Name = "txtLado";
+            this.txtLado.Size = new System.Drawing.Size(173, 32);
+            this.txtLado.TabIndex = 1;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(43, 103);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(110, 26);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Resultado";
+            this.lbl1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.Red;
+            this.lblResultado.Location = new System.Drawing.Point(43, 142);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(24, 26);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(43, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(145, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(247, 199);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 48);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 271);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtLado);
+            this.Controls.Add(this.lblLado);
+            this.Name = "Form1";
+            this.Text = "Exercício 02";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLado;
+        private System.Windows.Forms.TextBox txtLado;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Exercicio02/Exercicio02/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Limpando os valores
+            txtLado.Text = "";
+            lblResultado.Text = "0";
+
+            //Colocando o foco no componente do text box do lado
+            txtLado.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Fechar o formulário
+            this.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Declarar as variáreis
+            int lado;
+            int area;
+
+            //Capturando os valores de entrada
+            lado = Convert.ToInt32(txtLado.Text);
+
+            //Processamento
+
+            area = lado * lado;
+
+            //Saída
+            lblResultado.Text = Convert.ToString(area);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
